f2ac860d1b58a2851544520268a55c1727f612c1
Kirill Knyazkov <k.knyazkov@g.nsu.ru>
2024-09-22 23:03:09 +0700

    move and copy

--- lab1/hashtable.cpp
+++ lab1/hashtable.cpp
@@ -2,151 +2,193 @@
 #include <cstring>
 #include <iostream>
 #include <string>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
 typedef std::string Key;
 
-struct Value{
-    unsigned age;
-    unsigned weight;
+struct Value {
+  unsigned age;
+  unsigned weight;
 };
 
-class HashNode {
-    public:
-        Value value;
-        Key key;
-    
-    HashNode(Key key, Value value)
-    {
-        this->value = value;
-        this->key = key;
-    }
+struct HashNode {
+  Value value;
+  Key key;
+
+  HashNode(Key key, Value value) {
+    this->value = value;
+    this->key = key;
+  }
 };
 
-class HashTable{
-    private:
-        HashNode** table;
-        size_t capacity;
-        size_t size;
+class HashTable {
+private:
+  HashNode **table;
+  size_t capacity;
+  size_t size;
 
-        int hash(Key key) {
-            int sum = 0;
-            for (int i = 0; i < key.length(); i++)
-                sum += int(key[i]);
-            return sum % capacity;
-        }
-    public:
-        HashTable() : table(nullptr), capacity(0), size(0) {};
-        ~HashTable() {
-            for (size_t i = 0; i < capacity; ++i) {
-                delete table[i];
-            }
-            delete[] table;
-        }
+  int hash(Key key) {
+    int sum = 0;
+    for (int i = 0; i < key.length(); i++)
+      sum += int(key[i]);
+    return sum % capacity;
+  }
 
-        HashTable(size_t init_capacity) : capacity(init_capacity), size(0) {
-            table = new HashNode*[capacity];
-            for (int i = 0; i < capacity; i++)
-                table[i] = nullptr;
-        };
+public:
+  // Will cause floating point exception! (cuz its % by capacity that = 0)
+  HashTable() : table(nullptr), capacity(0), size(0) {};
+  ~HashTable() {
+    for (size_t i = 0; i < capacity; ++i) {
+      delete table[i];
+    }
+    delete[] table;
+  }
 
-        HashTable(const HashTable& other) : capacity(other.capacity), size(other.size) {
-            HashNode** newNode = new HashNode*[capacity];
-            for (int i = 0; i < size; i++)
-                newNode[i] = other.table[i];
-        }
-        HashTable(HashTable&& other) {
-            
-        }
+  HashTable(size_t init_capacity) : capacity(init_capacity), size(0) {
+    table = new HashNode *[capacity];
+    for (int i = 0; i < capacity; i++)
+      table[i] = nullptr;
+  };
 
-        void swap(HashTable& b);
+  HashTable(const HashTable &other)
+      : capacity(other.capacity), size(other.size) {
+    HashNode **newNode = new HashNode *[capacity];
+    for (int i = 0; i < size; i++)
+      newNode[i] = other.table[i];
+  };
 
-        //deep copy
-        HashTable& operator=(const HashTable& other) {
-            if (this == &other)
-                return *this;
+  HashTable(HashTable &&other) : capacity((other.capacity)), size(other.size) {
+    table = other.table;
+    size = other.size;
+    capacity = other.capacity;
 
-            for (size_t i = 0; i < capacity; ++i) {
-                delete table[i];
-            }
-            delete[] table;
+    other.table = nullptr;
+    other.capacity = 0;
+    other.size = 0;
+  };
 
-            capacity = other.capacity;
-            size = other.size;
-            table = new HashNode*[capacity];
-            for (size_t i = 0; i < capacity; ++i) {
-                table[i] = other.table[i];
-            }
+  // std::swap uses move semantic
+  // so it'll be expensive to copy and delete large amount of resources
+  // Custom one is simple pointer exchange
+  void swap(HashTable &other) {
+    HashNode **otherT = other.table;
+    other.table = table;
+    table = otherT;
 
-            return *this;
-        }
-        //
-        HashTable& operator=(HashTable&& other);
+    std::swap(other.capacity, capacity);
+    std::swap(other.size, size);
+  }
 
-        void clear();
-        bool erase(const Key& k);
-        bool insert(const Key& k, const Value& v);
+  // deep copy
+  HashTable &operator=(const HashTable &other) {
+    if (this == &other)
+      return *this;
 
-        bool contains(const Key& k) const;
+    for (size_t i = 0; i < capacity; ++i) {
+      delete table[i];
+    }
+    delete[] table;
 
-        Value& operator[](const Key& k);
+    capacity = other.capacity;
+    size = other.size;
+    table = new HashNode *[capacity];
+    for (size_t i = 0; i < capacity; ++i) {
+      table[i] = other.table[i];
+    }
+    return *this;
+  }
 
-        Value& at(const Key& k);
-        const Value& at(const Key& k) const;
+  HashTable &operator=(HashTable &&other) {
+    if (this != &other) {
 
-        size_t getSize() const;
-        bool empty() const;
+      for (size_t i = 0; i < capacity; ++i) {
+        delete table[i];
+      }
+      delete[] table;
 
-        friend bool operator==(const HashTable& a, const HashTable& b);
-        friend bool operator!=(const HashTable& a, const HashTable& b);
+      table = other.table;
+      size = other.size;
+      capacity = other.capacity;
+
+      other.table = nullptr;
+      other.capacity = 0;
+      other.size = 0;
+    }
+    return *this;
+  }
+
+  // clears table and sets size to 0
+  void clear();
+  bool erase(const Key &k);
+  // insertion using linear probing collision resolution
+  bool insert(const Key &k, const Value &v);
+
+  bool contains(const Key &k) const;
+
+  Value &operator[](const Key &k);
+
+  Value &at(const Key &k);
+  const Value &at(const Key &k) const;
+
+  size_t getSize() const;
+  bool empty() const;
+
+  friend bool operator==(const HashTable &a, const HashTable &b);
+  friend bool operator!=(const HashTable &a, const HashTable &b);
 };
 
-size_t HashTable::getSize() const{
-    return size;
+size_t HashTable::getSize() const { return size; }
+
+bool HashTable::empty() const { return size == 0; }
+
+// TODO: exception!
+// ALSO: it's super bad and dumb!
+Value &HashTable::at(const Key &k) {
+  int index = hash(k);
+  if (table[index] == nullptr) {
+    static Value dummy = {0, 0};
+    return dummy;
+  }
+  return table[index]->value;
 }
 
-bool HashTable::empty() const{
-    return size == 0;
+bool HashTable::contains(const Key &key) const { return at(key); }
+
+void HashTable::clear() {
+  for (size_t i = 0; i < capacity; ++i) {
+    delete table[i];
+  }
+  delete[] table;
+
+  size = 0;
 }
 
+bool HashTable::insert(const Key &k, const Value &v) {
+  if (size == capacity) {
+    // overflow!
+    return false;
+  }
 
-//TODO: exception!
-Value& HashTable::at(const Key& k){
-    int index = hash(k);
-    if (table[index] == nullptr){
-        static Value dummy = {0,0};
-        return dummy;
-        }
-    return table[index]->value;
+  HashNode *newNode = new HashNode(k, v);
+  int index = hash(k);
+
+  while (table[index] != nullptr && table[index]->key != k) {
+    index = (index + 1) % capacity;
+  }
+  
+  size++;
+  table[index] = newNode;
+  return true;
 }
 
-bool HashTable::contains(const Key& key) const{
-    return at(key);
-}
-
-//insertion using linear probing collision resolution
-bool HashTable::insert(const Key& k, const Value& v){
-    if (size == capacity) {
-        //overflow!
-        return false;
-    }
-
-    HashNode* newNode = new HashNode(k, v);
-    int index = hash(k);
-
-    while(table[index] != nullptr && table[index]->key != k){
-        index = (index + 1) % capacity;
-    }
-
-    if (table[index] == nullptr) size++;
-    table[index] = newNode;
-    return true;
-}
-
-bool HashTable::erase(const Key& k){
-    int id = hash(k);
-    delete table[id];
+bool HashTable::erase(const Key &k) {
+  // TODO: if not found by key then exception?
+  int id = hash(k);
+  delete table[id];
+  table[id] = nullptr;
+  return true;
 }
 
 /*
@@ -161,6 +203,4 @@ void HashTable::handleCollission(Key key, Value value, size_t sizeTable){
     table[index] = &newNode;
 }
 */
-int main(){
-    HashTable table(2);
-}
+int main() {}

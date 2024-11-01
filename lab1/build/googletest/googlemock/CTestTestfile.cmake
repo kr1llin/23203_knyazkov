5d58e6848a75be4bfc34866822c635ef4da55363
Kirill Knyazkov <k.knyazkov@g.nsu.ru>
2024-11-01 20:41:28 +0700

    a

--- lab1/build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/krillin/code/nsu/23203_knyazkov/lab1/googletest/googlemock
-# Build directory: /home/krillin/code/nsu/23203_knyazkov/lab1/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- lab1/build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/krillin/code/nsu/23203_knyazkov/lab1/googletest/googletest
-# Build directory: /home/krillin/code/nsu/23203_knyazkov/lab1/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
